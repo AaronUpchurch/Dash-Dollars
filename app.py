4c3552de2c4854c870dfaa0c7a144ab2ae0d1482
Aaron Upchurch <atu20@fsu.edu>
2025-04-27 17:21:12 -0400

    base dashboard

---
+++ app.py
@@ -0,0 +1,50 @@
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+from helper_functions import text_to_df
+
+# Initialize the app
+app = Dash()
+
+# App layout
+app.layout = [
+
+    # title
+    html.Div(children='Dash Dollars',style={'color': 'red', 'fontSize': 60, 'textAlign':'center',"font-weight": "bold"}),
+
+    # instructions
+    html.H3(children='1. Visit www.doordash.com and Sign in'),
+    html.H3(children='2. Click on \'Orders\''),
+    html.H3(children='3. Scroll to the Very Bottom'),
+    html.H3(children='4. Hit Ctrl+A and Ctrl+C'),
+    html.H3(children='5. Paste Copied Text into Textbox'),
+
+    
+    dcc.Textarea(
+        id='copy_input',
+        value='Copy and paste here',
+        style={'width': '20%', 'height': 30},
+    ),
+    
+    dcc.Graph(id='bar_plot'),
+    dcc.Graph(id='line_plot')
+
+]
+
+# Input Text Response
+@callback(
+    Output('bar_plot', 'figure'),
+    Output('line_plot', 'figure'),
+    Input('copy_input', 'value')
+)
+def update_output(text):
+    df = text_to_df(text)
+    group_by_resturant_df = df.groupby('Resturant').Price.sum().to_frame().reset_index().sort_values(by='Price',ascending=False)
+    bar_plot = px.bar(group_by_resturant_df, x='Resturant', y='Price')
+    line_plot = px.line(df, x='Date',y='running_total_price')
+    return bar_plot, line_plot
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helper_functions.py
@@ -0,0 +1,73 @@
+# text to dataframe function
+def text_to_df(text):
+
+    import pandas as pd
+    import numpy as np
+    from datetime import datetime
+    import re
+
+    # check that correct text has been inputed
+    if text[0:21] != 'LeftSideNavigationBar':
+        return pd.DataFrame(columns = ['Resturant','Date','Price','running_total_price'])
+
+    # remove header information
+    start_index = text.find('Orders\nCompleted') + len('Orders\nCompleted')
+    text = text[start_index:]
+
+    # add regex header to first order
+    header = '\nâ€¢ Leave a review'
+    text = header + text
+
+    # define order regex
+    regex = r'\• Leave a review\n([\w\s\']*)\n([{a-zA-z,1-9\s]*) • \$([1-9\.]*) •'
+    
+    # get regex matches
+    data = re.findall(regex,text)
+    
+    # convert to dateframe
+    df = pd.DataFrame(columns = ['Resturant','Date','Price'])
+    for i in range(len(data)): df.loc[len(df.index)] = [data[i][0],data[i][1],data[i][2]]
+    
+    # change datetype
+    df.Price = df.Price.astype(float)
+
+    # Issue: Year of order is not specified in order history. Must manually calcualte years.
+    
+    # define month int map
+    month_map = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
+    
+    # define current year and month
+    years = []
+    curr_year = datetime.now().year
+    curr_month = re.findall(r'\w\w\w, (\w\w\w)',df.iloc[0].Date)[0]
+    
+    # for each row calculate year
+    for i in range(len(df.index)-1):
+    
+        # record year
+        years = np.append(years,curr_year)
+    
+        # get following month
+        next_month = re.findall(r'\w\w\w, (\w\w\w)',df.iloc[i+1].Date)[0]
+    
+        # if next order's month is 'above' current order's month, a year has ended
+        if month_map[curr_month] < month_map[next_month]:
+            curr_year = curr_year - 1
+    
+        # move to next order
+        curr_month = next_month
+    
+    years = np.append(years,curr_year)
+    
+    years = np.array(years).astype(int)
+    
+    def make_datetime(x):
+        month = re.findall(r'\w\w\w, (\w\w\w)',x.Date)[0]
+        day = int(re.findall(r'\w\w\w, \w\w\w ([1-9]*)',x.Date)[0])
+        return datetime(years[x.name],month_map[month],day)
+    
+    df.Date = df.apply(make_datetime,axis=1)
+
+    df['running_total_price'] = [df.Price[i:].sum() for i in np.arange(0,df.shape[0])]
+
+    return df
